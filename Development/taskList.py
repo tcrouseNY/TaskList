734606792c15cffe4b575f6ec0a4105d71d024f0
tcrouseNY <todd.crouseNY@gmail.com>
2025-03-04 07:11:58 -0600

    first upload for Development branch

--- Development/taskList.py
+++ Development/taskList.py
@@ -10,6 +10,8 @@ dif userLogin(userID: str, userPassword: str): int
 
     return = 1
 
+
+
 ### Get a list of the projects in the DB
 dif GetProjectList(boCurrent: bool): list
     ### This that and the other stuff goes here.  Only return active projects!
